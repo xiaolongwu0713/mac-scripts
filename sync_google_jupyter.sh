770a0f43e3e274045e2ff559f89627bd30189f74
long <xiaolongwu0713@gmail.com>
2021-08-17 10:32:17 +0800

    modify sync TS_RNN.ipynb

--- sync_google_jupyter.sh
+++ sync_google_jupyter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source /usr/local/venv/3.6.9/bin/activate
-cd /Users/long/Documents/BCI/python_scripts/googleDrive/Colab\ Notebooks
-jupyter nbconvert --to script $1
+cd /Users/long/Documents/BCI/python_scripts/googleDrive/Colab\ Notebooks/grasp
+jupyter nbconvert --to script TS_RNN.ipynb
 suffix='.txt'
 new_suffix='.py'
-generated_file="$1${suffix}"
-new_file="/Users/long/Documents/BCI/python_scripts/googleDrive/grasp/TSception/$1${new_suffix}"
+generated_file="TS_RNN${suffix}"
+new_file="/Users/long/Documents/BCI/python_scripts/googleDrive/grasp/TSception/TS_RNN${new_suffix}"
 mv ${generated_file} ${new_file}
 #echo $generated_file
 sed -i -- 's/^%%/#%%/g'  $new_file
